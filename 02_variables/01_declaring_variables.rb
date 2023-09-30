e2b34aff54dac6dcf32d50d8ff7e9324c18a7d4b
Daniel Sousa <daniel.sousa@trinto.com.br>
2023-09-30 12:37:02 -0300

    declaring variables

---
+++ 02_variables/01_declaring_variables.rb
@@ -0,0 +1,27 @@
+age = 29
+puts age
+
+name = "Daniel "
+last_name = "Miranda"
+puts name
+puts last_name
+
+puts age + 7 # 29 + 7
+puts name + last_name
+
+age = 35
+
+puts age
+
+age_in_ten_years = age + 10
+
+puts age_in_ten_years
+
+age = age + 7 #42
+
+puts age
+
+chameleon = 24
+chameleon = "Some random text"
+chameleon = 3.14
+puts chameleon
