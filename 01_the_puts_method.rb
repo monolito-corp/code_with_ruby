23b740430d4e2a9b662ee032a7ce1fb4901c27e9
Daniel Sousa <daniel.sousa@trinto.com.br>
2023-09-30 10:34:32 -0300

    the puts method

---
+++ 01_the_puts_method.rb
@@ -0,0 +1,19 @@
+puts 1
+
+puts 1 + 2
+5
+puts(1 + 2)
+
+puts "Hello World"
+
+puts "I'm alive and well!"
+
+puts
+
+1
+
+puts "Afterwards"
+
+puts 4,5,1
+
+puts "Olá mundo", "velho", "Sem porteira"
