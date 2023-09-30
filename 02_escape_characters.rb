e51374872d406792a7c4e0ee5c58810a27db0d6a
Daniel Sousa <daniel.sousa@trinto.com.br>
2023-09-30 10:43:08 -0300

    scape characters

---
+++ 02_escape_characters.rb
@@ -0,0 +1,8 @@
+puts "Some text \nmore text"
+puts "\tOne upon a time"
+
+puts "Juliet said \"Goodbye\" to Romeo"
+puts 'Juliet said \'Goodbye\' to Romeo'
+
+puts 'Juliet said "Goodbye" to Romeo'
+puts "Juliet said 'Goodbye' to Romeo"
