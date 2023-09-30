864c17fca96cc476913e40889b80b851b1da7e6f
Daniel Sousa <daniel.sousa@trinto.com.br>
2023-09-30 13:02:24 -0300

    assignment shortcuts

---
+++ 02_variables/05_assignment_shortcuts.rb
@@ -0,0 +1,28 @@
+a = 10
+a = a + 5
+puts a
+
+###
+
+a = 10
+a += 5
+puts a 
+
+### 
+b = 100
+b = b - 40
+puts b
+
+
+###
+b = 100
+b -= 40
+puts b
+
+c = 3
+c = c * 4
+puts c
+
+c = 3
+c *= 4
+puts c
