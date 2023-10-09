6f5e6c35dda4b1d65bb0e5933e78688b492d508f
Daniel Miranda <daniel@monolito.io>
2023-10-09 08:36:08 -0300

    string interpolation

---
+++ 03_object_methods/07_string_interpolation.rb
@@ -0,0 +1,14 @@
+name = "Zé da manga"
+
+puts "Hello #{name}, how are you"
+
+age = 29
+
+puts "I am " + age.to_s + " years old"
+
+puts "I am #{age} years old"
+
+
+puts "In 5 years, I will be " + (age + 5).to_s 
+
+puts "In 5 years, I will be #{age + 5}"
