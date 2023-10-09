b5f75c2db1bcd912f74b42f9cd207bc8d5f5971f
Daniel Miranda <daniel@monolito.io>
2023-10-09 08:52:04 -0300

    the class method

---
+++ 03_object_methods/09_the_class_method.rb
@@ -0,0 +1,16 @@
+# Class is a blueprint for and object.
+# Object is and "instance" of the class
+
+puts "Hello".class
+puts "Ruby is fun".class
+puts "123$".class
+puts "".class
+
+puts 5.class
+puts -4.class
+puts 333.class
+puts 333_333_333.class
+
+puts 3.14.class
+puts 0.23.class
+puts -10.99.class
